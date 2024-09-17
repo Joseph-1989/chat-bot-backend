28bf67aa2f94813757797aa3669b18bd9dafdfad
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-09-17 10:15:50 +0900

    Initial commit with backend setup and .gitignore

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+let chatMessages = [];
+
+app.post("/message", (req, res) => {
+  const { message } = req.body;
+
+  chatMessages.push({ sender: "user", text: message });
+
+  const responseMessage = message + " + Hello world!";
+
+  chatMessages.push({ sender: "bot", text: responseMessage });
+
+  res.json({ reply: responseMessage, messages: chatMessages });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
